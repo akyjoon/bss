fb08dabf7e51c4f45cbfa1fd45f10e87feb81336
Angelo <akyjoon@gmail.com>
2018-02-12 05:15:30 +0100

    port change

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use('/priceList', priceList)
 
 
 //listen on port
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
